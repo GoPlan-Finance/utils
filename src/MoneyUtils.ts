f5dae44b88bfb15addc808ed32f3ebae35186d0f
Samuel Denis-D'Ortun <sam@sddproductions.com>
2021-08-07 23:54:51 -0400

    Add Money Utils

---
+++ src/MoneyUtils.ts
@@ -0,0 +1,22 @@
+import {Money} from 'ts-money'
+
+export class MoneyUtils {
+
+  public static fromAmount(rawVal : number | null): null | Money {
+    const val = Number.isInteger(rawVal) ? rawVal : null;
+
+    if (val === null || val === undefined) {
+      return null;
+    }
+
+    return new Money(val, 'CAD');
+  }
+
+  public static toAmount(val: Money | null): number | null {
+    if (val === null || val === undefined) {
+      return null
+    }
+
+    return val.getAmount();
+  }
+}
