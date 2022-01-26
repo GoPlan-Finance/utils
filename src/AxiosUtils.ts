967ecf505e988a5e0f67a9d63592dbe4306d5d4c
Samuel Denis-D'Ortun <sam@sddproductions.com>
2022-01-26 14:11:15 -0500

    Add missing return type

--- src/AxiosUtils.ts
+++ src/AxiosUtils.ts
@@ -1,15 +1,17 @@
-import axios, { CancelTokenSource } from 'axios';
+import axios, { AxiosResponse, CancelTokenSource } from 'axios';
 
 export class AxiosUtils {
-  public static axiosGetRequestWithAbortFactory() {
+  public static axiosGetRequestWithAbortFactory<T = never>(): (
+    url: string
+  ) => Promise<AxiosResponse<T>> {
     // eslint-disable-next-line init-declarations
     let call: CancelTokenSource = null;
-    return (url: string) => {
+    return (url: string): Promise<AxiosResponse<T>> => {
       if (call) {
         call.cancel();
       }
       call = axios.CancelToken.source();
-      return axios.get(url, { cancelToken: call.token });
+      return axios.get<T>(url, { cancelToken: call.token });
     };
   }
 }
